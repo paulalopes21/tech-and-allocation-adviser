9461fa76590121b547a5d973de0ad2b3ba91127b
Paula Lopes Silva <p000paula.lopes.silva@prservicos.com.br>
2019-01-24 10:04:16 -0200

    bootstrap do spring boot

---
+++ src/main/java/com/pls/techandallocationadviser/TechAndAllocationAdviserApplication.java
@@ -0,0 +1,14 @@
+package com.pls.techandallocationadviser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechAndAllocationAdviserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechAndAllocationAdviserApplication.class, args);
+	}
+
+}
+
